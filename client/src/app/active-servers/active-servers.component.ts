e7de21dfaee3e128c1d97249ef1e7ff7511a0a52
Dom <dominicsmith1021@gmail.com>
2019-10-04 19:57:51 -0400

    Added score and team logos

--- client/src/app/active-servers/active-servers.component.ts
+++ client/src/app/active-servers/active-servers.component.ts
@@ -2,13 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { preserveWhitespacesDefault } from '@angular/compiler';
 
+import { teams } from "../logos";
+
 @Component({
   selector: 'app-active-servers',
   templateUrl: './active-servers.component.html',
   styleUrls: ['./active-servers.component.css']
 })
+
 export class ActiveServersComponent implements OnInit {
     servers: any;
+    debounce: boolean;
+    cached: boolean;
 
     constructor(private http: HttpClient) {
     }
@@ -16,12 +21,37 @@ export class ActiveServersComponent implements OnInit {
     getServers() {
       let obs = this.http.get("/api/servers");
       obs.subscribe((resp: any[]) => { 
+        if (this.debounce) return;
+        this.debounce = true;
           resp.sort((a,b) => {return b.PlayerCount - a.PlayerCount});
-          this.servers = resp;
+          
+          if (this.cached) {
+            this.servers.forEach((server: Object) => {
+              for (let i = 0; i < resp.length; i++) {
+                if (resp[i].Id == server["Id"]) {
+                  server["Time"] = resp[i].Time;
+                }
+              }
+            })
+          } else {
+            resp = resp.map(server => {
+              console.log(teams[server.AwayName],server.AwayName,teams[server.HomeName],server.HomeName);
+              const obj = {...server,
+                AwayLogo: `https://i.cdn.turner.com/nba/nba/.element/img/1.0/teamsites/logos/teamlogos_500x500/${teams[server.AwayName]}.png`,
+                HomeLogo: `https://i.cdn.turner.com/nba/nba/.element/img/1.0/teamsites/logos/teamlogos_500x500/${teams[server.HomeName]}.png`
+              };
+              return obj;
+            });
+
+            this.servers = resp;
+            this.cached = true;
+          }
+          this.debounce = false;
         });
     }
 
     ngOnInit() {
         this.getServers();
+        // setInterval(() => {this.getServers()},1000);
     }
 }
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,7 +7,6 @@ module.exports = function(app) {
         axios.get("https://death-row-bot.herokuapp.com/livestats/623694595", {key: apiKey}).then(function(resp) {
             const response = resp.data;
             const data = response.data;
-            console.log("Route hit",data);
             res.json(data);
         });
     });
